aa26ad9bbf8daa8b5b9030c8775ccaf520679624
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-11-07 18:18:26 +0000

    Refactor regionValues() step 1

--- src/main/kotlin/sudoku/Sudoku.kt
+++ src/main/kotlin/sudoku/Sudoku.kt
@@ -48,20 +48,21 @@ fun columnValues(grid: List<Int>, columnIndex: Int): List<Int> {
 }
 
 fun regionValues(grid: List<Int>, columnIndex: Int, rowIndex: Int): List<Int> {
+    val startingRow = columnIndex / 3
+    val startingColumn = rowIndex / 3
+    val regionStartingIndex = ((startingColumn * 9) + startingRow) * 3
 
-    // TODO() refactor below
-
-    val regionIndices = when {
-        columnIndex <= 2 && rowIndex <= 2 -> listOf(0,1,2,9,10,11,18,19,20)
-        columnIndex <= 5 && rowIndex <= 2 -> listOf(3,4,5,12,13,14,21,22,23)
-        columnIndex <= 8 && rowIndex <= 2 -> listOf(6,7,8,15,16,17,24,25,26)
-        columnIndex <= 2 && rowIndex <= 5 -> listOf(27,28,29,36,37,38,45,46,47)
-        columnIndex <= 5 && rowIndex <= 5 -> listOf(30,31,32,39,40,41,48,49,50)
-        columnIndex <= 8 && rowIndex <= 5 -> listOf(33,34,35,42,43,44,51,52,53)
-        columnIndex <= 2 && rowIndex <= 8 -> listOf(54,55,56,63,64,65,72,73,74)
-        columnIndex <= 5 && rowIndex <= 8 -> listOf(57,58,59,66,67,68,75,76,77)
-        else -> listOf(60,61,62,69,70,71,78,79,80)
-    }
+    val regionIndices = listOf(
+        regionStartingIndex,
+        regionStartingIndex + 1,
+        regionStartingIndex + 2,
+        regionStartingIndex + (1 * 9),
+        regionStartingIndex + (1 * 9) + 1,
+        regionStartingIndex + (1 * 9) + 2,
+        regionStartingIndex + (2 * 9),
+        regionStartingIndex + (2 * 9) + 1,
+        regionStartingIndex + (2 * 9) + 2,
+    )
 
     return grid.filterIndexed { index, _ ->
         regionIndices.contains(index)
