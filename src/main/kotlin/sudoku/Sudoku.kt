c2b752f3c39aedc646f86a97e2d4f6b82ed87278
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-11-11 18:59:31 +0000

    Create grid and create possible grid methods

--- src/main/kotlin/sudoku/Sudoku.kt
+++ src/main/kotlin/sudoku/Sudoku.kt
@@ -7,13 +7,11 @@ fun solveSudoku(grid: List<Int>): List<Int> {
         if (it == 0) calculateValue(grid, index) else listOf(it)
     }
 
+    val newGrid = sudokuTry.map { if (it.size == 1) it.single() else 0 }
+
     return if (sudokuTry.all { it.size == 1 }) {
         sudokuTry.flatten()
     } else {
-        val newGrid = sudokuTry
-            .toMutableList()
-            .map { if (it.size > 1) 0 else it.single() }
-
         if (newGrid == grid) newGrid else solveSudoku(newGrid)
     }
 }
@@ -29,7 +27,7 @@ fun calculateValue(grid: List<Int>, index: Int): List<Int> {
 }
 
 fun possibleRowValues(grid: List<Int>, rowIndex: Int): List<Int> =
-    listOf(1,2,3,4,5,6,7,8,9)
+    listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
         .subtract(
             grid.subList(
                 (rowIndex * ROW_LENGTH),
@@ -54,11 +52,20 @@ fun regionValues(grid: List<Int>, columnIndex: Int, rowIndex: Int): List<Int> {
 
     val regionTopRow = (regionStartingIndex..(regionStartingIndex + 2)).toList()
     val regionIndices =
-            regionTopRow +
-            regionTopRow.map { it + ROW_LENGTH } +
-            regionTopRow.map { it + (2* ROW_LENGTH) }
+        regionTopRow +
+                regionTopRow.map { it + ROW_LENGTH } +
+                regionTopRow.map { it + (2 * ROW_LENGTH) }
 
     return grid.filterIndexed { index, _ ->
         regionIndices.contains(index)
     }
 }
+
+fun createGrid(grid: List<Int>, gridIndex: Int, value: Int): List<Int> =
+    grid.mapIndexed { index, it -> if (index == gridIndex) value else it }
+
+fun createPossibleGrids(
+    grid: List<Int>,
+    pairOfIndexAndPossibleValues: Pair<Int, List<Int>>
+): List<List<Int>> =
+    pairOfIndexAndPossibleValues.second.map { createGrid(grid, pairOfIndexAndPossibleValues.first, it) }
--- src/test/kotlin/sudoku/SudokuKtTest.kt
+++ src/test/kotlin/sudoku/SudokuKtTest.kt
@@ -35,7 +35,7 @@ class SudokuKtTest {
     }
 
     @Test
-    fun `should calculate 9x9 grid with one empty grid position in first row`() {
+    fun `should solve puzzle with one empty grid position in first row`() {
         val input = singleEmptyGridValueIsSixAtIndexFive
 
         val result = solveSudoku(input)
@@ -44,7 +44,7 @@ class SudokuKtTest {
     }
 
     @Test
-    fun `should calculate single empty value in second row in 9x9 grid`() {
+    fun `should calculate single empty value in second row in 9x9 puzzle`() {
         val grid = singleEmptyGridValueIsThreeAtIndexTwelve
 
         val result = calculateValue(grid, 12)
@@ -140,5 +140,45 @@ class SudokuKtTest {
         val result = solveSudoku(input)
 
         assertThat(result).isEqualTo(puzzleWithMoreThanOneSolutionOutput)
+    }
+
+//    @Test
+//    fun `should calculate full hard, solvable puzzle`() {
+//        val input = hardPuzzle
+//
+//        val result = solveSudoku(input)
+//
+//        assertThat(result).isEqualTo(hardPuzzleSolution)
+//    }
+
+    @Test
+    fun `should create grid replacing index with indexed value`() {
+        val grid = multipleValueOptionsAtIndexThirtyFiveCorrectIsOne
+        val expected = listOf(
+            7, 2, 3, 8, 4, 6, 1, 5, 9,
+            6, 1, 5, 3, 9, 2, 4, 7, 8,
+            8, 4, 9, 7, 1, 5, 6, 3, 0,
+            3, 7, 8, 6, 5, 4, 9, 0, 1,
+            1, 9, 4, 2, 8, 7, 3, 6, 5,
+            2, 5, 6, 9, 3, 1, 8, 4, 7,
+            5, 6, 1, 4, 7, 9, 2, 8, 3,
+            4, 8, 7, 1, 2, 3, 5, 9, 6,
+            9, 3, 2, 5, 6, 8, 7, 1, 4
+        )
+
+        val result = createGrid(grid, 35, 1)
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should return list of possible grids`() {
+        val startingGrid = multipleValueOptionsAtIndexThirtyFiveCorrectIsOne
+        val possibleGrid1 = multipleValueOptionsAtIndexThirtyFiveCorrectIsOneCorrectOption1
+        val possibleGrid2 = multipleValueOptionsAtIndexThirtyFiveCorrectIsOneIncorrectOption2
+
+        val result = createPossibleGrids(startingGrid, Pair(35, listOf(1, 2)))
+
+        assertThat(result).containsOnly(possibleGrid1, possibleGrid2)
     }
 }
